741728d17973ef8545b01e0541835e14555b3a85
James Chen <unknownguy2002@gmail.com>
2018-12-14 12:32:29 +0800

    Security improvements and /unshorten command

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var localtunnel = require('localtunnel');
 var express = require('express');
 var app = express()
 var bodyParser = require('body-parser')
+var crypto = require('crypto')
+var qs = require('qs')
 const config = require("./config.json")
+const slackSigningSecret = config.signingsecret
 app.use(bodyParser());
 
 var tunnel = localtunnel(config.port, {"subdomain":config.url}, function(err, tunnel) {
@@ -21,22 +24,63 @@ app.get('/shorten', function(req, res){
 });
 
 app.post('/shorten', function(req, res){
-    let args = req.body.text.split(" ")
-    console.log("Shortening")
-    if(!args[0] ||!args[1]){
-        res.send('Missing arguments');
+    //verify
+    let slackSignature = req.headers['x-slack-signature'];
+    let requestBody = qs.stringify(req.body,{ format:'RFC1738' });
+    let timestamp = req.headers['x-slack-request-timestamp'];
+    let sigBasestring = 'v0:' + timestamp + ':' + requestBody;
+    let mySignature = 'v0='+crypto.createHmac('sha256', slackSigningSecret).update(sigBasestring, 'utf8').digest('hex');
+    if(slackSignature != mySignature){
+        console.log("Faked signature!!!!")
         return;
     }
-    res.send('Attemping to shorten! WoOoOoOoOoOoT');
-    fetch(`${config.yourl}?signature=${config.signature}&action=shorturl&format=json&url=${args[0]}&keyword=${args[1]}`).then(getres => getres.json()).then(json => {
+    //
+
+    let args = req.body.text.split(" ")
+    res.setHeader('Content-Type', 'application/json');
+    if(!args[0] ||!args[1]){
+        res.send(JSON.stringify({"response_type": "in_channel","text": "Missing Arguments"}));
+        return;
+    }
+    res.send(JSON.stringify({"response_type": "in_channel","text": "Verified. Attemping to shorten! :link:"}));
+    fetch(`${config.yourl}/yourls-api.php?signature=${config.signature}&action=shorturl&format=json&url=${args[0]}&keyword=${args[1]}`).then(getres => getres.json()).then(json => {
         if(json.status === "fail"){
             fetch(req.body.response_url, { method: 'POST', body: JSON.stringify({"response_type": "in_channel","text": `Error: ${json.message}`,}), headers: { 'Content-Type': 'application/json' }})
         }   
         if(json.status === "success"){
-            fetch(req.body.response_url, { method: 'POST', body: JSON.stringify({"response_type": "in_channel","text": `Success: ${json.message}\nKeyword: ${json.url.keyword}`,}), headers: { 'Content-Type': 'application/json' }})
+            fetch(req.body.response_url, { method: 'POST', body: JSON.stringify({"response_type": "in_channel","text": `Success: ${json.message}\nShortcut: ${config.yourl}${json.url.keyword}`,}), headers: { 'Content-Type': 'application/json' }})
         } 
     })
 });
 
+app.post('/unshorten', function(req, res){
+    //verify
+    let slackSignature = req.headers['x-slack-signature'];
+    let requestBody = qs.stringify(req.body,{ format:'RFC1738' });
+    let timestamp = req.headers['x-slack-request-timestamp'];
+    let sigBasestring = 'v0:' + timestamp + ':' + requestBody;
+    let mySignature = 'v0='+crypto.createHmac('sha256', slackSigningSecret).update(sigBasestring, 'utf8').digest('hex');
+    if(slackSignature != mySignature){
+        console.log("Faked signature!!!!")
+        return;
+    }
+    //
+    
+    let args = req.body.text.split(" ")
+    res.setHeader('Content-Type', 'application/json');
+    if(!args[0]){
+        res.send(JSON.stringify({"response_type": "in_channel","text": "Missing Arguments"}));
+        return;
+    }
+    res.send(JSON.stringify({"response_type": "in_channel","text": "Verified. Attemping to unshorten! :boom:"}));
+    fetch(`${config.yourl}/yourls-api.php?signature=${config.signature}&action=delete&shorturl=${args[0]}&format=json`).then(getres => getres.json()).then(json => {
+        if(json.statusCode != 200){
+            fetch(req.body.response_url, { method: 'POST', body: JSON.stringify({"response_type": "in_channel","text": `${json.message.charAt(0).toUpperCase() + json.message.slice(1)}`,}), headers: { 'Content-Type': 'application/json' }})
+        }   
+        if(json.statusCode === 200){
+            fetch(req.body.response_url, { method: 'POST', body: JSON.stringify({"response_type": "in_channel","text": `${json.message.charAt(0).toUpperCase() + json.message.slice(1)}`,}), headers: { 'Content-Type': 'application/json' }})
+        } 
+    })
+});
 app.listen(80)
 
