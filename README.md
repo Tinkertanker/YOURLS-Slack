36f1035871a5b644e398d4631adfc9af137d44db
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-03-04 11:04:34 +0800

    Reformat code
    No functional changes.

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # YOURLS-Slack
+
 A YOURLS Slack bot for people who simply despise their browsers. Simple script
 
 ## How to install:
+
 <details><summary>Show</summary>
 <p>
 Documentation is WIP
 
 #### Step 1: Clone the repo & install dependencies using npm install
+
 ![alt Clone and Instal dependencies](https://raw.githubusercontent.com/Tinkertanker/YOURLS-Slack/master/docs/images/1.1.png)
 
-
 #### Step 2: Create a slack application
+
 ![alt Create by signing in and going to https://api.slack.com/apps](https://raw.githubusercontent.com/Tinkertanker/YOURLS-Slack/master/docs/images/1.2.png)
 
-
 #### Step 3: Create commands
+
 ![alt Create /shorten](https://raw.githubusercontent.com/Tinkertanker/YOURLS-Slack/master/docs/images/1.3.png)
 ![alt Create /unshorten](https://raw.githubusercontent.com/Tinkertanker/YOURLS-Slack/master/docs/images/1.4.png)
-
 
 #### Step 4: Fill in the exampleconfig.json file, rename it to config.json and run node index
 
@@ -25,22 +27,28 @@ Documentation is WIP
 </details>
 
 ## Credits:
+
 ### Node.js
+
 - ~Localtunnel(https://www.npmjs.com/package/localtunnel) because Slack's /commands requires a http endpoint.~
 - Express.js for recieving post requests from slack.
 - Node-fetch(https://www.npmjs.com/package/node-fetch) for API calls.
 - Crypto & qs(https://www.npmjs.com/package/qs) for slack verification
 - This awesome article, https://medium.com/@rajat_sriv/verifying-requests-from-slack-using-node-js-69a8b771b704 from which I copied wholesomely from :)
 - ~Supervisor(https://www.npmjs.com/package/supervisor) in case Localtunnel decides to crash.~
+
 ### YOURLs
+
 - YOURLS Plugin: API Action - Delete (https://github.com/claytondaley/yourls-api-delete)
 
 ### People
+
 - Liew Zhao Wei
 
 All at Tinkertanker.
 
 ## License:
+
 ```
 MIT License
 
@@ -64,5 +72,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ```
+
 ### Made by:
+
 <img src="https://raw.githubusercontent.com/Tinkertanker/YOURLS-Slack/master/docs/images/tticon.png" alt="Tinkertanker" width="50%" height="50%">
--- index.js
+++ index.js
@@ -1,75 +1,142 @@
-const fetch = require('node-fetch');
-var express = require('express');
-var app = express()
-var bodyParser = require('body-parser')
-var crypto = require('crypto')
-var qs = require('qs')
-const config = require("./config.json")
-const slackSigningSecret = config.signingsecret
+const fetch = require("node-fetch");
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var crypto = require("crypto");
+var qs = require("qs");
+const config = require("./config.json");
+const slackSigningSecret = config.signingsecret;
 app.use(bodyParser());
 
-app.get('/shorten', function(req, res){
-    console.log("Get Shortening")
+app.get("/shorten", function(req, res) {
+  console.log("Get Shortening");
 });
 
-app.post('/shorten', function(req, res){
-    //verify
-    let slackSignature = req.headers['x-slack-signature'];
-    let requestBody = qs.stringify(req.body,{ format:'RFC1738' });
-    let timestamp = req.headers['x-slack-request-timestamp'];
-    let sigBasestring = 'v0:' + timestamp + ':' + requestBody;
-    let mySignature = 'v0='+crypto.createHmac('sha256', slackSigningSecret).update(sigBasestring, 'utf8').digest('hex');
-    if(slackSignature != mySignature){
-        console.log("Faked signature!!!!")
-        return;
-    }
-    //
+app.post("/shorten", function(req, res) {
+  //verify
+  let slackSignature = req.headers["x-slack-signature"];
+  let requestBody = qs.stringify(req.body, { format: "RFC1738" });
+  let timestamp = req.headers["x-slack-request-timestamp"];
+  let sigBasestring = "v0:" + timestamp + ":" + requestBody;
+  let mySignature =
+    "v0=" +
+    crypto
+      .createHmac("sha256", slackSigningSecret)
+      .update(sigBasestring, "utf8")
+      .digest("hex");
+  if (slackSignature != mySignature) {
+    console.log("Faked signature!!!!");
+    return;
+  }
+  //
 
-    let args = req.body.text.split(" ")
-    res.setHeader('Content-Type', 'application/json');
-    if(!args[0] ||!args[1]){
-        res.send(JSON.stringify({"response_type": "in_channel","text": "Missing Arguments"}));
-        return;
-    }
-    res.send(JSON.stringify({"response_type": "in_channel","text": "Verified. Attemping to shorten! :link:"}));
-    fetch(`${config.yourl}/yourls-api.php?signature=${encodeURI(config.signature)}&action=shorturl&format=json&url=${encodeURI(args[0])}&keyword='${encodeURI(args[1].toLowerCase())}'`).then(getres => getres.json()).then(json => {
-        if(json.status === "fail"){
-            fetch(req.body.response_url, { method: 'POST', body: JSON.stringify({"response_type": "in_channel","text": `Error: ${json.message}`,}), headers: { 'Content-Type': 'application/json' }})
-        }   
-        if(json.status === "success"){
-            fetch(req.body.response_url, { method: 'POST', body: JSON.stringify({"response_type": "in_channel","text": `Success: ${json.message}\nShortcut: ${json.shorturl}`,}), headers: { 'Content-Type': 'application/json' }})
-        } 
+  let args = req.body.text.split(" ");
+  res.setHeader("Content-Type", "application/json");
+  if (!args[0] || !args[1]) {
+    res.send(
+      JSON.stringify({ response_type: "in_channel", text: "Missing Arguments" })
+    );
+    return;
+  }
+  res.send(
+    JSON.stringify({
+      response_type: "in_channel",
+      text: "Verified. Attemping to shorten! :link:"
     })
+  );
+  fetch(
+    `${config.yourl}/yourls-api.php?signature=${encodeURI(
+      config.signature
+    )}&action=shorturl&format=json&url=${encodeURI(
+      args[0]
+    )}&keyword='${encodeURI(args[1].toLowerCase())}'`
+  )
+    .then(getres => getres.json())
+    .then(json => {
+      if (json.status === "fail") {
+        fetch(req.body.response_url, {
+          method: "POST",
+          body: JSON.stringify({
+            response_type: "in_channel",
+            text: `Error: ${json.message}`
+          }),
+          headers: { "Content-Type": "application/json" }
+        });
+      }
+      if (json.status === "success") {
+        fetch(req.body.response_url, {
+          method: "POST",
+          body: JSON.stringify({
+            response_type: "in_channel",
+            text: `Success: ${json.message}\nShortcut: ${json.shorturl}`
+          }),
+          headers: { "Content-Type": "application/json" }
+        });
+      }
+    });
 });
 
-app.post('/unshorten', function(req, res){
-    //verify
-    let slackSignature = req.headers['x-slack-signature'];
-    let requestBody = qs.stringify(req.body,{ format:'RFC1738' });
-    let timestamp = req.headers['x-slack-request-timestamp'];
-    let sigBasestring = 'v0:' + timestamp + ':' + requestBody;
-    let mySignature = 'v0='+crypto.createHmac('sha256', slackSigningSecret).update(sigBasestring, 'utf8').digest('hex');
-    if(slackSignature != mySignature){
-        console.log("Faked signature!!!!")
-        return;
-    }
-    //
-    
-    let args = req.body.text.split(" ")
-    res.setHeader('Content-Type', 'application/json');
-    if(!args[0]){
-        res.send(JSON.stringify({"response_type": "in_channel","text": "Missing Arguments"}));
-        return;
-    }
-    res.send(JSON.stringify({"response_type": "in_channel","text": "Verified. Attemping to unshorten! :boom:"}));
-    fetch(`${config.yourl}/yourls-api.php?signature=${encodeURI(config.signature)}&action=delete&shorturl='${encodeURI(args[0].toLowerCase())}'&format=json`).then(getres => getres.json()).then(json => {
-        if(json.statusCode != 200){
-            fetch(req.body.response_url, { method: 'POST', body: JSON.stringify({"response_type": "in_channel","text": `${json.message.charAt(0).toUpperCase() + json.message.slice(1)}`,}), headers: { 'Content-Type': 'application/json' }})
-        }   
-        if(json.statusCode === 200){
-            fetch(req.body.response_url, { method: 'POST', body: JSON.stringify({"response_type": "in_channel","text": `${json.message.charAt(0).toUpperCase() + json.message.slice(1)}`,}), headers: { 'Content-Type': 'application/json' }})
-        } 
+app.post("/unshorten", function(req, res) {
+  //verify
+  let slackSignature = req.headers["x-slack-signature"];
+  let requestBody = qs.stringify(req.body, { format: "RFC1738" });
+  let timestamp = req.headers["x-slack-request-timestamp"];
+  let sigBasestring = "v0:" + timestamp + ":" + requestBody;
+  let mySignature =
+    "v0=" +
+    crypto
+      .createHmac("sha256", slackSigningSecret)
+      .update(sigBasestring, "utf8")
+      .digest("hex");
+  if (slackSignature != mySignature) {
+    console.log("Faked signature!!!!");
+    return;
+  }
+  //
+
+  let args = req.body.text.split(" ");
+  res.setHeader("Content-Type", "application/json");
+  if (!args[0]) {
+    res.send(
+      JSON.stringify({ response_type: "in_channel", text: "Missing Arguments" })
+    );
+    return;
+  }
+  res.send(
+    JSON.stringify({
+      response_type: "in_channel",
+      text: "Verified. Attemping to unshorten! :boom:"
     })
+  );
+  fetch(
+    `${config.yourl}/yourls-api.php?signature=${encodeURI(
+      config.signature
+    )}&action=delete&shorturl='${encodeURI(args[0].toLowerCase())}'&format=json`
+  )
+    .then(getres => getres.json())
+    .then(json => {
+      if (json.statusCode != 200) {
+        fetch(req.body.response_url, {
+          method: "POST",
+          body: JSON.stringify({
+            response_type: "in_channel",
+            text: `${json.message.charAt(0).toUpperCase() +
+              json.message.slice(1)}`
+          }),
+          headers: { "Content-Type": "application/json" }
+        });
+      }
+      if (json.statusCode === 200) {
+        fetch(req.body.response_url, {
+          method: "POST",
+          body: JSON.stringify({
+            response_type: "in_channel",
+            text: `${json.message.charAt(0).toUpperCase() +
+              json.message.slice(1)}`
+          }),
+          headers: { "Content-Type": "application/json" }
+        });
+      }
+    });
 });
 app.listen(config.port, () => console.log(`Started server on ${config.port}`));
-
